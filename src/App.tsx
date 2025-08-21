e4cac7371fdbc8593e245321159764d55ad6686f
kojosh <jdko@uci.edu>
2025-08-20 17:11:48 -0700

    adjust layout and add links/files

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,34 @@
 import './App.css'
+import profileImage from "./assets/SF_pfp.jpg";
 
 function App() {
 
   return (
-    <>
-      <div className="container">
+      <div className="homepage">
         {/* Name */}
-        <h1 className="name">Joshua Ko</h1>
+        <div className="card">
+          <img src={profileImage} alt="Profile" className="profile-img" />
 
-        {/* About Me */}
-        <p className="about">
-          I am a fourth-year computer engineering student at the University of California, Irvine. <br />
-          I am interested in web development and chip architecture and I am currently looking for internships.
-        </p>
+          <div className="info">
+            <h1>Joshua Ko</h1>
+            <p>
+              I am a fourth-year computer engineering student at the University of California, Irvine. <br />
+              I am interested in web development and chip architecture and I am currently looking for internships.
+            </p>
 
-        {/* Links */}
-        <div className="links">
-          <a 
-            href="/resume.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn"
-          >
-            View Resume
-          </a>
-          <a 
-            href="https://github.com/kojosh"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn"
-          >
-            GitHub
-          </a>
+            <div className="links">
+              <a href="/JoshuaKo-Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a> |
+              <a href="https://www.linkedin.com/in/joshua-doha-ko" target="_blank"> Linkedin</a> |
+              <a href="https://github.com/kojosh" target="_blank"> Github</a>
+            </div>
+
+            <div className="email">
+              <span>📧</span>
+              <a href="mailto:joshuadohako@gmail.com">joshuadohako@gmail.com</a>
+            </div>
+          </div>
         </div>
       </div>
-    </>
   )
 }
 
