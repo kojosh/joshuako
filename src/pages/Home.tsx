7427e57cb0bb981738cd01e32c9cc392d9129907
kojosh <kojosh07@gmail.com>
2026-08-27 17:21:47 -0700

    modify link colors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,8 +17,10 @@ export default function Home() {
         <div className="info">
           <p>
             I am a computer engineering student at the University of California, Irvine. I am interested in embedded systems, system software, and robotics. <br />
-            I am an undergraduate researcher at the <a href="https://aicps.eng.uci.edu/" target="_blank" rel="noopener noreferrer">AICPS Lab </a> 
-            and SDE intern at <a href="https://www.amazon.com/" target="_blank" rel="noopener noreferrer">Amazon</a>.
+            I am an undergraduate researcher at the{" "}
+            <a href="https://aicps.eng.uci.edu/" target="_blank" rel="noopener noreferrer">AICPS Lab</a>{" "}
+            and SDE intern at{" "}
+            <a href="https://www.amazon.com/" target="_blank" rel="noopener noreferrer">Amazon</a>.
             Please feel free to check out my projects and reach out to me if you have any questions or opportunities. <br />
           </p>
         </div>
